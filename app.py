d867f192d044322d2ea890a792ba982648bc5bec
Emre PEKER <emrepkrr@gmail.com>
2026-07-15 13:17:17 +0300

    Add division operation

--- app.py
+++ app.py
@@ -6,5 +6,10 @@ def substract(a,b):
 def muliply(a,b):
     return a * b
 
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Divisor cannot be zero")
+
+    return a / b 
 if __name__ == "__main__":
     print(add(2,3))
